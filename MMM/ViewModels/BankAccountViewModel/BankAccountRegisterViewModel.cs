78eea791a304c13e90c088c5ef8814c2a5caf676
Bartek Bienczyk <bartlomiej.bienczyk@gmail.com>
2017-06-19 18:47:58 +0200

    I've changed Create View and I've added ViewModel of BankAccount.

---
+++ MMM/ViewModels/BankAccountViewModel/BankAccountRegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MMM.ViewModels.BankAccountViewModel
+{
+    public class BankAccountRegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Nazwa konta")]
+        public string Name { get; set; }
+        [Display(Name = "Saldo początkowe")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public decimal Balance { get; set; }
+        [Required]
+        [Display(Name = "Waluta")]
+        public int Currency { get; set; }
+
+    }
+}
